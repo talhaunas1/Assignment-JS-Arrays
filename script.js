0e6ed8f7b57b1b5434bfbf2c61614e0b4e4fda5e
Muhammad Talha <mtalha4600@gmail.com>
2022-01-04 00:01:14 +0500

    javascript arrays

--- script.js
+++ script.js
@@ -1,108 +1,220 @@
-// 1. Write a js program to read and print elements of array. 
-
-// var array = [1, 2, 3, 4, 5]
-// console.log("Array", array);
+ //1. Write a js program to read and print elements of array.
 
 
-// 2. Write a js program to print all negative elements in an array. 
+//  var arr = [21,43,23,54,67,65];
 
-// var myArray = [1, 2, -56, 45, -634, -568, 106, 434];
+//  console.log("showing array element " + arr);
 
-// function getNegativeNumbers(array) {
 
-//     var negatives = [];
+//------------------------------------------------------------------------------------------------------------------------------
 
-//     for (var i = 0; i < array.length; i++) {
-//         if (array[i] < 0) {
-//             negatives.push(array[i]);
+//Write a js program to print all negative elements in an array
+
+//  var arr = [32,-112,43,65,-24,-35,23];
+
+//  console.log("the array values are " + arr);
+ 
+//  for(var i=0;i<6;i++)
+//  {
+     
+//      if( arr[i]<0)
+//      {
+//          console.log("negative values"+arr[i]);
+//      }
+     
+//  }
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+// 3:Write a js program to find sum of all array elements.
+
+// var array=[1,2,3,4,5,6]
+// var sum=0;
+
+// console.log("before sum the values are " + array)
+//  for (let index = 0; index < array.length; index++) 
+//  {
+//      sum += array[index];
+     
+//  }
+//  console.log("after sum the array value is " +sum);
+
+//------------------------------------------------------------------------------------------------------------------------------
+ 
+// 4.Write a js program to find maximum and minimum element in an array.
+
+//  function max_min(){
+//     var arr = [2,421,5,75,1,24,23,34];
+//     var max=min= arr[0];
+//     // var min = arr[0];
+// console.log(arr)
+//     for (let i = 0; i< arr.length; i++)
+//      {
+//         if(arr[i]>max) 
+//         {
+//             max=arr[i];
+//         }
+//         if (min>arr[i])
+//         {
+//             min=arr[i];
 //         }
 //     }
-//     return negatives;
+//     console.log("the max is "+max);
+//     console.log("the min is "+min);
 
+
+//     // document.write(`the largest element in array is ${max}`) 
 // }
-// console.log(getNegativeNumbers(myArray));
+// max_min();
 
 
 
-// 3. Write a js program to find sum of all array elements.  
+//--------------------------------------------------------------------------------------------------------------------------------------
+//5. Write a js program to find second largest element in an array.
 
-// var array = [1, 2, 3, 4, 5, 6];
-// var sum = 0;
-// for (var i = 0; i < array.length; i++) {
-//     sum += array[i];
+// var arr=[21,543,653,34,23,65,87,98,45,34]
+// var largest;
+// var second_largest;
+// var int_min;                                     //not solved
+
+// largest=second_largest=int_min;
+
+// for(let i=0;i<arr.length;i++)
+// {
+//     if(arr[i]>largest)
+//     {
+//         second_largest = largest;
+//         largest=arr[i];
+//     }
+//     else if(arr[i]<largest&& arr[i]>second_largest)
+//     {
+//         second_largest=arr[i]
+//     }
 // }
-// console.log("Sum of array is :", sum);
+// console.log("the largest is " + largest + " and the seond largest is "+ second_largest)
+
+//------------------------------------------------------------------------------------------------------------------------------
+//6. Write a js program to count total number of even and odd elements in an array.
 
 
-// 4. Write a js program to find maximum and minimum element in an array. 
+// var arr=[21,42,34,55,56,75,31,0,29,54]
+// var even;
+// var odd;
+// console.log(arr)
+// for (let i = 0; i < arr.length; i++) 
+// {
+//     console.log(arr[i])
 
-// var Array = [1, 5, 6, 2, 3,87,-5,-4];
-// var result = Math.min(...Array);
-// console.log("Minimum element is",result)
-
-// var result2 = Math.max(...Array)
-// console.log("Maximum element is",result2);
-
-
-
-
-// 6. Write a js program to count total number of even and odd elements in an array. 
-
-// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 87];
-// var odd = 0;
-// var even = 0;
-// for (var x = 0; x <array.length; x++) {
-//   if (array[x] % 2 == 0) {
-//     even++;
-//   }
-//   if (array[x] % 2 != 0) {
-//     odd++;
-//   }
+//     if (arr[i]%2==0) 
+//     {
+//         even=arr[i]  
+//         console.log("the "+even+" number is even");
+//     }
+//     if (arr[i]%2==1) {
+        
+//         odd=arr[i]
+//         console.log("the "+ odd +" number is odd" )
+//     }
 // }
-// console.log("Total number of Even", even);
-// console.log("Total number of Odd", odd);
 
 
-// 7. Write a js program to count total number of negative elements in an array. 
-
-// var negArray = [2, 3 -21, 5, -8, -4, 3, -90];
-// var negElement = 0;
-// for (var x = 0; x < negArray.length; x++) {
-//   if (negArray[x] < 0) {
-//     negElement++;
-//   }
+//----------------------------------------------------------with function--------------------------------------------------------------------
+// var arr=[21,42,34,55,56,12,342,75,31,0,29,54]
+// var even,odd
+// console.log(arr)
+// function even_odd(params) 
+// {
+    
+//     for (let i = 0; i < arr.length; i++) 
+//     {
+//         if (arr[i]%2==0) 
+//         {
+//             even=arr[i]
+//             console.log("the "+even+ " is even")
+//         }
+//         if (arr[i]%2==1)
+//         {
+//             odd=arr[i]  
+//             console.log("the "+odd+ " is odd")
+//         }
+//     }    
 // }
-// console.log("Total number Of negative elements in array is", negElement);
+// even_odd()
 
+//------------------------------------------------------------------------------------------------------------------------------
+//7. Write a js program to count total number of negative elements in an array.
 
-// 8. Write a js program to copy all elements from an array to another array. 
-
-// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// var copyarray = [];
-// for (var x = 0; x < array.length; x++) {
-//   copyarray.push(array[x]);
+// var arr=[32,-3,42,64,-12,42,-156,09,-43]
+// var count=0,i
+// function find_negative(params) 
+// {
+//     console.log(arr);
+//     for ( i = 0; i < arr.length; i++) 
+//     {
+//         if (arr[i]<0)
+//         {
+//             console.log(arr[i])
+            
+//         }
+//         if(arr[i] < 0)
+//         {
+//             count++;
+//         }
+        
+//     }
+   
+//     console.log("Total negative elements in array "+ count);
 // }
-// console.log("Copy Of Array Is ", copyarray);
+// find_negative()
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+// 8.Write a js program to copy all elements from an array to another array.
+
+
+// function copy_element()
+// {
+   
+//  var arr = [1,3,6,7,8,9,4,56,75];
+// var copy_array = [];
+// for (var i = 0; i < arr.length; i++) {
+//   copy_array.push(arr[i]);
+// }
+// console.log("Copy Of Array Is ", copy_array);
+// }
+// copy_element();
 
 
 
-// 9. Write a js program to insert an element in an array. 
+//------------------------------------------------------------------------------------------------------------------------------
+//9. Write a js program to insert an element in an array.
 
-// var inarray = [1, 2, 3, 4, 5, 6];
-// console.log("Before insert", inarray);
-// inarray.push(-7);
-// console.log("After insert", inarray);
+// var arr = [12,3,53,23,63,23,20]
+// console.log("before inserting value " + arr)
 
+// var new_array = parseInt(prompt("Enter value you want to insert: "))
+// var newar =  parseInt(prompt("Enter value you want to insert: "))
+
+// console.log("after inserting")
+// arr.push(new_array,newar)
+// console.log(arr)
+//------------------------------------------------------------------------------------------------------------------------------
 
 // 10. Write a js program to delete an element from an array at specified position.
 
+// var arr = [1,2,3,4,5,6,7,8,9]
 
-// var delarray = [1, 2, 3, 4, 5, 6, 7, 8];
-// console.log("Before deletion", delarray);
-//   delarray.splice(2, 1);
-//   console.log("Delete element at specified position", delarray);
+// console.log("before deleting ", arr)
+
+// var arr_after= arr.splice(4,3)
+
+// console.log("after deleting")
+
+// console.log(arr)
 
 
+//------------------------------------------------------------------------------------------------------------------------------
 // 11. Write a js program to count frequency of each element in an array.
 
 
@@ -119,40 +231,56 @@
 // }
 
 
-
+//------------------------------------------------------------------------------------------------------------------------------
 // 12. Write a js program to print all unique elements in the array.
 
-// var uniarray = [1, 2, 3, 4, 5, 2, 1];
-// if (uniarray == 0) {
-//     console.log("There is no element in array");
-// } else {
-//     for (var x = 0; x < uniarray.length; x++) {
-//         var unique = 0;
-//         for (var y = 0; y < uniarray.length; y++) {
-//             if (uniarray[x] == uniarray[y]) {
-//                 unique++;
-//             }
+// var array=[21,33,44,55,33,23,33,66,44,55]
+// console.log(array)
+// for (let i = 0; i < array.length; i++) 
+// {
+//     var count=0
+//     for (let j = 0; j < array.length; j++) {
+        
+//         if (array[i]==array[j]) 
+//         {
+//             count++ 
 //         }
-
-//         if (unique == 1) {
-//             console.log(uniarray[x], " is unique element in array");
-//         }
+       
+//     }
+//     if (count== 1) 
+//     {
+//         console.log(array[i]);
 //     }
 // }
 
-// 13. Write a js program to count total number of duplicate elements in an array.
+//------------------------------------------------------------------------------------------------------------------------------
+//13. Write a js program to count total number of duplicate elements in an array.
 
-// var duparray = [1, 2, 3, 4, 1, 2,3,3,4,6,6];
-// var duplication = 0;
-// for (var x = 0; x < duparray.length; x++) {
-//   for (var y = x + 1; y < duparray.length; y++) {
-//     if (duparray[x] == duparray[y]) {
-//       duplication++;
-//       duparray.splice(y, 1);
+// var array=[22,33,44,54,33,22,11,3,3,4,65,11]
+// console.log(array)
+
+// var count=0
+// for (let i = 0; i < array.length; i++) {
+    
+//     for (let j = i+1; j < array.length; j++) {
+        
+//         if (array[i]==array[j])
+//          {
+//             count++
+//         }
+        
 //     }
-//   }
+    
 // }
-// console.log("total number of duplicate elements in an array is ", duplication);
+// console.log(count)
+
+
+
+
+
+
+
+
 
 
 
